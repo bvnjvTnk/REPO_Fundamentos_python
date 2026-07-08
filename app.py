dbb5e6618b153d7fff6cdb81c186d750fbb37dbb
bvnjvTnk <ben.barriosc@duocuc.cl>
2026-07-07 22:36:11 -0400

    validaciones basicas
    todas las condiciones que debe tener los datos

--- app.py
+++ app.py
@@ -31,30 +31,54 @@ def leer_opcion():
         except ValueError:
             print("Ingresa una opcion valida")
 
-def stock_categoria(categoria,productos,inventario):
-    pass
+
+def validar_codigo(codigo):
+    return codigo.strip() != ""
+
+def validar_nombre(nombre):
+    return nombre.strip() != ""
 
 
-def buscar_precio(precio_min, precio_max, productos, inventario):
-    pass
-
-def buscar_codigo(codigo, productos):
-    pass
-
-def agregar_producto(codigo, nombre, categoria, precio, disponible, stock, vendidos, productos,inventario):
-    pass
+def validar_categoria(categoria):
+    return categoria.strip() != ""
 
 
-def eliminar_producto(codigo, productos, inventario):
-    pass
+def validar_precio(precio):
+    return precio >0
 
-def mostrar_productos(productos, inventario):
-    pass
+
+def validar_disponible(disponible):
+    if disponible == "s":
+        return True
+    elif disponible == "n":
+        return False
+    else:
+        print("Dato no valido")
+        return disponible
+    
+def validar_stock(stock):
+    return stock >=0
+
+
+def validar_vendidos(vendidos):
+    return vendidos >=0
+
+
+
 
 
 def main():
-    productos = {}
-    inventario = {}
+    productos = {
+"P101":["Cuaderno","Papelería",2490,True],
+"P102":["Lápiz","Papelería",590,True],
+"P103":["Botella","Accesorios",6990,False],
+"P104":["Mochila","Accesorios",24990,True]}
+    inventario = {
+    "P101":[30,15],
+    "P102":[120,50],
+    "P103":[0,10],
+    "P104":[8,25]
+    }
 
     mostrar_menu()
     leer_opcion()
